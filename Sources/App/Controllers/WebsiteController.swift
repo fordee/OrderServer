313749ed7a0b8879b69a0668a4ed976fd660be1b
John Forde <fordee@me.com>
2022-06-19 11:39:58 +1200

    Listing out the products.

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -8,12 +8,19 @@
 import Vapor
 import Leaf
 
+struct IndexContext: Encodable {
+  let title: String
+  let products: [Product]?
+}
+
 struct WebsiteController: RouteCollection {
   func boot(routes: RoutesBuilder) throws {
     routes.get(use: indexHandler)
   }
 
   func indexHandler(_ req: Request) async throws -> View {
-    return try await req.view.render("index")
+    let products = try await Product.query(on: req.db).all()
+    let context = IndexContext(title: "Home Page", products: products)
+    return try await req.view.render("index", context)
   }
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -24,8 +24,6 @@ public func configure(_ app: Application) throws {
   
   app.views.use(.leaf)
   
-  
-  
   // register routes
   try routes(app)
 }
