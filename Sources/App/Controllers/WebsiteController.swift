52ae89756e6258db32efef56882211095e7691ae
John Forde <fordee@me.com>
2022-06-19 13:51:09 +1200

    Added Bootstrap and tidied things up.

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -13,14 +13,28 @@ struct IndexContext: Encodable {
   let products: [Product]?
 }
 
+struct ProductContext: Encodable {
+  let title: String
+  let product: Product
+}
+
 struct WebsiteController: RouteCollection {
   func boot(routes: RoutesBuilder) throws {
     routes.get(use: indexHandler)
+    routes.get("products", ":productID", use: productHandler)
   }
 
   func indexHandler(_ req: Request) async throws -> View {
     let products = try await Product.query(on: req.db).all()
     let context = IndexContext(title: "Home Page", products: products)
     return try await req.view.render("index", context)
+  }
+
+  func productHandler(_ req: Request) async throws -> View {
+    guard let product = try await Product.find(req.parameters.get("productID"), on: req.db) else {
+      throw Abort(.notFound)
+    }
+    let context = ProductContext(title: product.name, product: product)
+    return try await req.view.render("product", context)
   }
 }
